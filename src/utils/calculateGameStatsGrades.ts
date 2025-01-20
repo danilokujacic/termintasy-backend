0e741ccf1929347b61b382ac098cba2a33ddfb51
danilokujacic <danilo.kujacic01@gmail.com>
2025-01-20 12:55:20 +0100

    game stats grades

---
+++ src/utils/calculateGameStatsGrades.ts
@@ -0,0 +1,44 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+export default async function calculateGrade() {
+  const gameStats = await prisma.gameStat.findMany({
+    include: {
+      player: true,
+    },
+  });
+  for (const gameStat of gameStats) {
+    const { position } = gameStat.player;
+
+    if (
+      gameStat.goal >= 5 ||
+      (position === 'GK' && gameStat.saves >= 15) ||
+      gameStat.assists >= 3 ||
+      gameStat.cleanSheet >= 1
+    ) {
+      return await this.prisma.gameStat.update({
+        where: { id: gameStat.id },
+        data: {
+          grade: 'S',
+        },
+      });
+    } else if (
+      gameStat.goal >= 2 ||
+      (position === 'GK' && gameStat.saves >= 10) ||
+      gameStat.assists >= 1
+    ) {
+      return await this.prisma.gameStat.update({
+        where: { id: gameStat.id },
+        data: {
+          grade: 'A',
+        },
+      });
+    } else {
+      return await this.prisma.gameStat.update({
+        where: { id: gameStat.id },
+        data: {
+          grade: 'F',
+        },
+      });
+    }
+  }
+}
