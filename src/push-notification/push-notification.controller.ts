fae67ae6135a08f3a21688b77c3ec83d591ef863
danilokujacic <danilo.kujacic01@gmail.com>
2025-01-22 11:33:58 +0100

    add auth guard to save token route

--- src/push-notification/push-notification.controller.ts
+++ src/push-notification/push-notification.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { PushNotificationService } from './push-notification.service';
 import { TokenDTO } from 'src/types';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('push-notification')
 export class PushNotificationController {
   constructor(private pushService: PushNotificationService) {}
 
+  @UseGuards(AuthGuard)
   @Post('save-token')
   async saveToken(@Body() tokenPayload: TokenDTO, @Req() request: any) {
     return this.pushService.saveToken({
